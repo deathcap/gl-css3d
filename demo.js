3c3c0de98f9dea6869f9ea22976ee458a1c6cf6f
deathcap <deathcap@gmx.co.uk>
2015-02-14 22:43:21 -0800

    Change to mesh example

--- demo.js
+++ demo.js
@@ -1,30 +1,59 @@
 "use strict"
-// based on example from https://github.com/mikolalysenko/draw-billboard
+
 var shell = require("gl-now")({clearColor: [0.2, 0.4, 0.8, 1.0]})
 var camera = require("game-shell-orbit-camera")(shell)
 var renderText = require("gl-render-text")
 var mat4 = require("gl-mat4")
-var drawBillboard = require("draw-billboard")
+var createMesh = require("gl-mesh")
+var glslify = require("glslify")
 
 var texture
 var positions = new Array(100)
+var mesh
+var shader
 
 shell.on("gl-init", function() {
   var gl = shell.gl
   texture = renderText(gl, "Billboard")
 
-  for(var i=0; i<100; ++i) {
-    positions[i] = [ 100 * (0.5 - Math.random()),
-                     100 * (0.5 - Math.random()),
-                     100 * (0.5 - Math.random()) ]
-  }
+  mesh = createMesh(gl, require("bunny"))
+
+  shader = glslify({
+    inline: true,
+    vertex: "/* voxel-decals vertex shader */\
+attribute vec3 position;\
+\
+uniform mat4 projection;\
+uniform mat4 view;\
+uniform mat4 model;\
+\
+void main() {\
+  gl_Position = projection * view * model * vec4(position, 1.0);\
+}",
+
+  fragment: "/* voxel-decals fragment shader */\
+precision highp float;\
+\
+void main() {\
+  gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);\
+}"})(gl)
+
 })
+
+var model = mat4.create()
+mat4.translate(model, model, [0,-5,20])
 
 shell.on("gl-render", function() {
   var proj = mat4.perspective(mat4.create(), Math.PI/4.0, shell.width/shell.height, 0.1, 1000.0)
   var view = camera.view()
 
-  for(var i=0; i<100; ++i) {
-    drawBillboard(shell.gl, positions[i], { texture: texture, projection: proj, view: view })
-  }
+  shader.bind()
+  shader.attributes.position.location = 0
+  shader.uniforms.projection = proj
+  shader.uniforms.view = view
+  shader.uniforms.model = model
+
+  mesh.bind(shader)
+  mesh.draw()
+  mesh.unbind()
 })
