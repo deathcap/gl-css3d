7996e8e38ea3555c27526533ab7bf57aa29d6528
deathcap <deathcap@gmx.co.uk>
2015-02-20 23:32:59 -0800

    Use simple-3d-shader to shade the bunny, size and move it into position

--- demo.js
+++ demo.js
@@ -6,6 +6,7 @@ var camera = require("game-shell-orbit-camera")(shell)
 var mat4 = require("gl-mat4")
 var bunny = require("bunny")
 var createMesh = require("gl-mesh")
+var createSimpleShader = require("simple-3d-shader")
 
 var iframe = document.createElement('iframe');
 iframe.src = 'http://browserify.org';
@@ -24,7 +25,7 @@ camera.lookAt([0,0,-3], [0,0,0], [0,1,0])
 // a slight rotation for effect
 camera.rotate([1/4,-1/4,0], [0,0,0])
 
-var bunnyMesh
+var bunnyMesh, bunnyShader
 
 shell.on('gl-init', function() {
   // allow pointer events to pass through canvas to CSS world behind
@@ -34,6 +35,7 @@ shell.on('gl-init', function() {
   css3d.ginit(shell.gl);
 
   bunnyMesh = createMesh(shell.gl, bunny);
+  bunnyShader = createSimpleShader(shell.gl)
 });
 
 var button = document.createElement('button');
@@ -52,13 +54,26 @@ shell.on('gl-resize', function(width, height) {
   css3d.updatePerspective(cameraFOVradians, shell.width, shell.height);
 });
 
+var bunnyModelMatrix = mat4.create();
+mat4.scale(bunnyModelMatrix, bunnyModelMatrix, [1/10, 1/10, 1/10]);
+mat4.translate(bunnyModelMatrix, bunnyModelMatrix, [-15, 0, 0]); // slightly overlapping gl-css3d
+var bunnyColor = [1, 0, 0]; // solid red, no texture
+
 shell.on('gl-render', function() {
   var proj = mat4.perspective(mat4.create(), cameraFOVradians, shell.width/shell.height, 0.1, 1000.0)
   var view = camera.view()
 
   css3d.render(view, proj);
 
-  bunnyMesh.bind(css3d.cutoutShader); // TODO: better shader
+  // draw another 3D object so the demo is more convincing
+  bunnyShader.bind();
+  bunnyShader.uniforms.projection = proj;
+  bunnyShader.uniforms.view = view;
+  bunnyShader.uniforms.model = bunnyModelMatrix;
+  bunnyShader.attributes.position.location = 0
+  bunnyShader.attributes.color = bunnyColor;
+
+  bunnyMesh.bind(bunnyShader);
   bunnyMesh.draw();
   bunnyMesh.unbind();
 })
